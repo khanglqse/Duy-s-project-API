e545cb48cdc994dd94ec597f8b2e839d23e93fad
Anh Trieu Quang <anh.trieuquang.ext@tba.group>
2023-03-20 14:23:33 +0700

    Update pharmacy viewModel

--- ViewModels/Pharmacy/PharmacyViewModel.cs
+++ ViewModels/Pharmacy/PharmacyViewModel.cs
@@ -6,6 +6,7 @@ namespace DuyProject.API.ViewModels.Pharmacy
         public string Name { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public List<string> DrugIds { get; set; } = new List<string>();
+        public string DoctorId { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public string LogoId { get; set; } = string.Empty;
         public bool IsActive { get; set; }
