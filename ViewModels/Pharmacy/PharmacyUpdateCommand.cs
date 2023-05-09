594f2588c802fdb49261bc8e05a3a08057f8877e
An Mai Thien <maian1891@gmail.com>
2023-05-09 09:22:30 +0700

    update pharmacy

--- ViewModels/Pharmacy/PharmacyUpdateCommand.cs
+++ ViewModels/Pharmacy/PharmacyUpdateCommand.cs
@@ -2,6 +2,7 @@ namespace DuyProject.API.ViewModels.Pharmacy
 {
     public class PharmacyUpdateCommand : PharmacyCreateCommand
     {
+        public string Id { get; set; }
         public List<string> FollowUser { get; set; }
     }
 }
