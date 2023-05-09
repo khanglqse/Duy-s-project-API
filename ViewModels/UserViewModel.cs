4985e99f115f414988dfacc51274a5f9ef657042
An Mai Thien <maian1891@gmail.com>
2023-05-09 09:40:44 +0700

    update userviewmodel

--- ViewModels/UserViewModel.cs
+++ ViewModels/UserViewModel.cs
@@ -5,13 +5,18 @@ namespace DuyProject.API.ViewModels;
 public class UserViewModel
 {
     public string Id { get; set; } = string.Empty;
+
     public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; }
     public bool IsActive { get; set; }
     public bool IsCreateBySocialAccount { get; set; } = false;
+    public string City { get; set; } = string.Empty;
+    public string Street { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public string ZipCode { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
     public string Type { get; set; } = "Point";
     public double[] Coordinates { get; set; }   
-    public string ZipCode { get; set; } = string.Empty;
     public string Roles { get; set; } = string.Empty;
     public string Phone { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
